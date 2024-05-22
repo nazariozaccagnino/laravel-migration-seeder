d59a2283d4ce1a01f6b31cdfa75b31d02ec61aca
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-05-22 17:43:50 +0200

    controller created

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    $products = config('db.products');
-    return view('home', compact('products'));
-})->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 
 Route::get('/about', function () {
