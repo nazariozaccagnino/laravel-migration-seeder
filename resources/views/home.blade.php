12802b4cd2bc6521199d2db8ad57e72d4f290f2a
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-05-22 18:42:31 +0200

    final commit with some style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,14 +5,40 @@
 @section('content')
 <section class="container">
     <h1>Trains</h1>
-    @foreach ($trains as $train)
-    <ul>
-        <li>
-            {{$train->company}}
-        </li>
-    </ul>
     
+    <table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Azienda</th>
+      <th scope="col">Stazione di partenza</th>
+      <th scope="col">Stazione di arrivo</th>
+      <th scope="col">Partenza</th>
+      <th scope="col">Arrivo</th>
+      <th scope="col">Numero treno</th>
+      <th scope="col">Vagoni totali</th>
+      <th scope="col">In ritardo</th>
+      <th scope="col">Cancellato</th>
+    </tr>
+  </thead>
+  <tbody>
+  @foreach ($trains as $train)
+    <tr>
+      <th scope="row">{{$train->id}}</th>
+      <td>{{$train->company}}</td>
+      <td>{{$train->start_station}}</td>
+      <td>{{$train->end_station}}</td>
+      <td>{{$train->departure}}</td>
+      <td>{{$train->arrival}}</td>
+      <td>{{$train->train_number}}</td>
+      <td>{{$train->wagons}}</td>
+      <td>{{$train->on_time}}</td>
+      <td>{{$train->deleted}}</td>
+    </tr>
     @endforeach
+</table>
+    
+   
 </section>
 
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,7 @@
 <footer>
-    Sono footer
+    <div class="container">
+        <div class="text-center">
+            Created by Nazario Zaccagnino - 22/05/2024
+        </div>
+    </div>
 </footer>
