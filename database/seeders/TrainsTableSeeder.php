a770872596a4071503b9e596752dd632972a2158
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-05-23 17:02:37 +0200

    created the seed with fakerPHP

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -5,24 +5,28 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Train;
+use Faker\Generator as Faker;
 
 class TrainsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
+        for($i=0; $i<7; $i++){
         $new_train = new Train();
-        $new_train->company = 'Italo';
-        $new_train->start_station = 'Milano Centrale';
-        $new_train->end_station = 'Torino PN';
-        $new_train->departure = '2024-05-24 12:00:00';
-        $new_train->arrival = '2024-05-24 13:30:00';
-        $new_train->train_number = 8870;
-        $new_train->wagons = 8;
-        $new_train->on_time = 1;
-        $new_train->deleted = 0;
+        $new_train->company = $faker->company();
+        $new_train->start_station = $faker->city();
+        $new_train->end_station = $faker->city();
+        $new_train->departure = $faker->dateTimeBetween('now', '+1 day');
+        $new_train->arrival = $faker->dateTimeBetween('now', '+1 day');
+        $new_train->train_number = $faker->randomNumber(4, true);
+        $new_train->wagons = $faker->numberBetween(1, 20);
+        $new_train->on_time = $faker->numberBetween(0,1);
+        $new_train->deleted = $faker->numberBetween(0,1);
         $new_train->save();
+        }
+        
     }
 }
